3b50bc2eeb2124651445623d37760ac280125df2
dribble-njr <wzw15292257101@163.com>
2025-06-18 19:57:09 +0800

    chore: default theme

--- apps/mobile/constants/Themes.ts
+++ apps/mobile/constants/Themes.ts
@@ -6,7 +6,107 @@ import { configureFonts, MD3DarkTheme, MD3LightTheme } from 'react-native-paper'
 import { Colors } from '@/constants/Colors'
 
 // TODO: change font
-const fonts = configureFonts({ config: {}, isV3: true })
+// #region custom fonts
+const fontConfig = {
+  default: {
+    fontSize: 14,
+    fontWeight: '400',
+    letterSpacing: 0,
+  },
+  displayLarge: {
+    fontSize: 57,
+    fontWeight: '400',
+    letterSpacing: 0,
+    lineHeight: 64,
+  },
+  displayMedium: {
+    fontSize: 45,
+    fontWeight: '400',
+    letterSpacing: 0,
+    lineHeight: 52,
+  },
+  displaySmall: {
+    fontSize: 36,
+    fontWeight: '400',
+    letterSpacing: 0,
+    lineHeight: 44,
+  },
+  headlineLarge: {
+    fontSize: 32,
+    fontWeight: '400',
+    letterSpacing: 0,
+    lineHeight: 40,
+  },
+  headlineMedium: {
+    fontSize: 28,
+    fontWeight: '400',
+    letterSpacing: 0,
+    lineHeight: 36,
+  },
+  headlineSmall: {
+    fontSize: 24,
+    fontWeight: '400',
+    letterSpacing: 0,
+    lineHeight: 32,
+  },
+  titleLarge: {
+    fontSize: 22,
+    fontWeight: '400',
+    letterSpacing: 0,
+    lineHeight: 28,
+  },
+  titleMedium: {
+    fontSize: 16,
+    fontWeight: '500',
+    letterSpacing: 0.15,
+    lineHeight: 24,
+  },
+  titleSmall: {
+    fontSize: 14,
+    fontWeight: '500',
+    letterSpacing: 0.1,
+    lineHeight: 20,
+  },
+  labelLarge: {
+    fontSize: 14,
+    fontWeight: '500',
+    letterSpacing: 0.1,
+    lineHeight: 20,
+  },
+  labelMedium: {
+    fontSize: 12,
+    fontWeight: '500',
+    letterSpacing: 0.5,
+    lineHeight: 16,
+  },
+  labelSmall: {
+    fontSize: 11,
+    fontWeight: '500',
+    letterSpacing: 0.5,
+    lineHeight: 16,
+  },
+  bodyLarge: {
+    fontSize: 16,
+    fontWeight: '400',
+    letterSpacing: 0.15,
+    lineHeight: 24,
+  },
+  bodyMedium: {
+    fontSize: 14,
+    fontWeight: '400',
+    letterSpacing: 0.25,
+    lineHeight: 20,
+  },
+  bodySmall: {
+    fontSize: 12,
+    fontWeight: '400',
+    letterSpacing: 0.4,
+    lineHeight: 16,
+  },
+} as const
+// #endregion
+
+const fonts = configureFonts({ config: fontConfig, isV3: true })
 
 const BaseLightTheme = {
   ...MD3LightTheme,
