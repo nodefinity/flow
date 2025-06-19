1083d6b246e696473285d2f0757da36c23397d67
dribble-njr <wzw15292257101@163.com>
2025-06-19 23:22:11 +0800

    chore: bump deps

--- apps/mobile/app/(tabs)/index.tsx
+++ apps/mobile/app/(tabs)/index.tsx
@@ -1,17 +1,75 @@
-import { StyleSheet } from 'react-native'
-import { TracksScreen } from '@/components/screens'
-import { ThemedView } from '@/components/ui/ThemedView'
+import type { Track } from '@flow/core'
+import { useTrackStore } from '@flow/core'
+import { FlashList } from '@shopify/flash-list'
+import { Image } from 'expo-image'
+import { StyleSheet, View } from 'react-native'
+import { IconButton, List, Text } from 'react-native-paper'
+import { usePlayerControl } from '@/hooks/usePlayerControl'
 
 export default function HomeScreen() {
+  const { tracks } = useTrackStore()
+  const { playList } = usePlayerControl()
+
+  const renderItem = ({ item }: { item: Track }) => (
+    <List.Item
+      title={item.title}
+      description={`${item.artist} - ${item.album}`}
+      descriptionNumberOfLines={1}
+      style={{ paddingLeft: 16, paddingRight: 8 }}
+      left={() => <Image source={{ uri: item.artwork }} style={{ aspectRatio: 1, borderRadius: 10 }} />}
+      right={() => (
+        <View style={styles.rightContent}>
+          <IconButton
+            icon="plus"
+            size={14}
+            onPress={() => console.log('Pressed')}
+          />
+
+          <IconButton
+            icon="dots-vertical"
+            size={14}
+            onPress={() => console.log('Pressed')}
+          />
+        </View>
+      )}
+      onPress={() => {
+        playList(item, tracks)
+      }}
+    />
+  )
+
   return (
-    <ThemedView style={styles.container}>
-      <TracksScreen />
-    </ThemedView>
+    <>
+      {
+        tracks.length > 0
+          ? (
+              <FlashList
+                data={tracks}
+                renderItem={renderItem}
+                keyExtractor={item => item.id}
+                showsVerticalScrollIndicator={false}
+                extraData={tracks}
+              />
+            )
+          : (
+              <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <Text>No songs</Text>
+              </View>
+            )
+      }
+    </>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  rightContent: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  duration: {
+    opacity: 0.6,
   },
 })
